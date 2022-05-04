d59601b1e1a0a23e6298ffebaf6ddc8d9e4c2931
bryanhancco <bhanccoco@unsa.edu.pe>
2022-05-04 16:38:47 -0500

    modificando para mostrar nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
 	public static void main(String[] args) {
-		System.out.println("Bienvenido al curso");
+		System.out.println("Hola soy Bryan Orlando Hancco Condori");
 	}
 }
